c3b074222cb78f6c6b613153ee53ccc9386f083c
nataliat11 <tomadon.nat@gmail.com>
2020-12-11 14:27:16 -0300

    Entidade Carro

---
+++ src/main/java/com/everis/projeto/demo/entities/Carro.java
@@ -0,0 +1,99 @@
+package com.everis.projeto.demo.entities;
+
+
+import java.io.Serializable;
+
+public class Carro implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String placa;
+	private String cor;
+	
+	private Modelo modelo;
+	private Marca marca;
+	
+	public Carro() {
+	}
+
+	public Carro(Long id, String placa, String cor, Modelo modelo, Marca marca) {
+		this.id = id;
+		this.placa = placa;
+		this.cor = cor;
+		this.modelo = modelo;
+		this.marca = marca;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public Modelo getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(Modelo modelo) {
+		this.modelo = modelo;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carro other = (Carro) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
+	
+	
+	
+	
+
+}
---
+++ src/test/java/com/everis/projeto/demo/ProjetoEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.projeto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
